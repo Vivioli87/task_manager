3e5b6c88496d164792e24c990c1522ba9a4854cf
Vivioli87 <vivnightingale@hotmail.com>
2021-07-02 22:17:04 +0000

    Create the Flask App

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+# to use environment variables need to import env package
+# env.py wont be pushed to github...
+# once app deployed to Heroku it wont find env.py and throw error
+# why only need to import env if the os can find existing file path for env.py
+if os.path.exists("env.py"):
+    import env
+
+# instance of Flask stored in variable app
+app = Flask(__name__)
+
+
+# / refers to default route
+@app.route("/")
+def hello():
+    return "Hello World... again!"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
