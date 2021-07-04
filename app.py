bc12ea1363f834220ce7b8787a9a6f5cf9990f47
Vivioli87 <vivnightingale@hotmail.com>
2021-07-04 01:00:56 +0000

    Add edit category functionality

--- app.py
+++ app.py
@@ -172,6 +172,21 @@ def add_category():
 
     return render_template("add_category.html")
 
+
+@app.route("/edit_category/<category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    if request.method == "POST":
+        submit = {
+            "category_name": request.form.get("category_name")
+        }
+        mongo.db.categories.update({"_id": ObjectId(category_id)}, submit)
+        flash("Category Succesfully Updated")
+        return redirect(url_for("get_categories"))
+
+    category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
+    return render_template("edit_category.html", category=category)
+
+
 # tells our app how & where to run the app
 # gets IP and PORT variables from env.py file
 # only use 'debug=True' during development, change to False before submission
