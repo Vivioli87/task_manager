c6a1df5ad764339951bd02a18e8d89115fced7af
Vivioli87 <vivnightingale@hotmail.com>
2021-07-04 00:25:45 +0000

    Add manage categories template

--- app.py
+++ app.py
@@ -154,6 +154,11 @@ def delete_task(task_id):
     return redirect(url_for("get_tasks"))
 
 
+@app.route("/get_categories")
+def get_categories():
+    categories = list(mongo.db.categories.find().sort("category_name", 1))
+    return render_template("categories.html", categories=categories)
+
 # tells our app how & where to run the app
 # gets IP and PORT variables from env.py file
 # only use 'debug=True' during development, change to False before submission
